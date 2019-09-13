31c101474cefe51ee164318a9a9f39c46ecdb239
THKonst <work-tk@yandex.ru>
2019-09-13 21:50:20 +0300

    added all to add item activity

--- app/src/main/java/com/thvkonst/mycartlist/Add_ItemActivity.java
+++ app/src/main/java/com/thvkonst/mycartlist/Add_ItemActivity.java
@@ -2,11 +2,13 @@ package com.thvkonst.mycartlist;
 
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,7 +17,10 @@ public class Add_ItemActivity extends AppCompatActivity {
     private static final String TAG = "Add_ItemActivity";
     private EditText name;
     private EditText price;
+    private TextView smbRbl;
     private Button addBtn;
+    private String msg_name="", msg_price="";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,25 +28,27 @@ public class Add_ItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add__item);
 
         setTitle(R.string.add_item_title);
+
         name = findViewById(R.id.name);
         price = findViewById(R.id.price);
         addBtn = findViewById(R.id.add_btn);
+        smbRbl = findViewById(R.id.smb_rbl);
 
         name.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int i, int i1, int i2) {
+            public void beforeTextChanged(CharSequence c, int i, int i1, int i2) {
 
             }
 
             @Override
-            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+            public void onTextChanged(CharSequence c, int i, int i1, int i2) {
 
             }
 
             @Override
-            public void afterTextChanged(Editable s) {
-                Log.i(TAG, "afterTextChanged: "+ s);
-
+            public void afterTextChanged(Editable c) {
+                Log.i(TAG, "afterNameChanged: " + c);
+                addBtn.setEnabled(!TextUtils.isEmpty(c));
             }
         });
 
